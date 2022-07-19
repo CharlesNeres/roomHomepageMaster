d79fc332b84fb3a6932cb45a5741220689226e0b
Charles Júnior <charlesbritoneresjr@gmail.com>
2022-07-20 00:02:28 +0800

    implemented header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import './App.css';
-
+import logo from './images/logo.svg';
+import iconMenu from './images/icon-hamburger.svg';
+import angleLeft from './images/icon-angle-left.svg'
+import angleRigth from './images/icon-angle-right.svg'
 function App() {
   return (
     <div className="App">
+      {/* ---------------- header */}
+      <header>
+        <nav>
+          <img src={iconMenu} className="iconMenu" />
+          <a href="">
+            <img src={logo} />
+          </a>
+        </nav>
+        <div className='btns'>
+          <button>
+            <img src={angleLeft} />
+          </button>            
+          <button>
+            <img src={angleRigth} /> 
+          </button>
+        </div>
+      </header>
     </div>
   );
 }

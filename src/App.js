eabcba827dff781949af6ad41e1189725d41318c
Charles Júnior <charlesbritoneresjr@gmail.com>
2022-07-20 00:18:46 +0800

    created home page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './images/logo.svg';
 import iconMenu from './images/icon-hamburger.svg';
 import angleLeft from './images/icon-angle-left.svg'
 import angleRigth from './images/icon-angle-right.svg'
+import iconArrow from './images/icon-arrow.svg';
 function App() {
   return (
     <div className="App">
@@ -23,6 +24,22 @@ function App() {
           </button>
         </div>
       </header>
+      <main>
+        <h1>Discover innovative ways to de decorate</h1>
+        <p>
+          We provide unmatched quality, comfort, and style 
+          for proerty owners across the country.
+          Our experts combine farm and function in 
+          bringing your vision to life. Create a room
+          in your own style with our collection and make 
+          your property a reflection of you and what you love.
+        </p>
+
+        <button>
+          shop now 
+          <img src={iconArrow} />
+        </button>
+      </main>
     </div>
   );
 }
